77c437a024e26d188cc33b61731a5958c7eb8d94
KirillAndreevichBarsukov <barsukov.kirill.a@gmail.com>
2024-07-03 13:13:11 +0300

    creating the program structure, initial setting of the main window, adding fonts

---
+++ hook/useFetch.js
@@ -0,0 +1,8 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const UseFetch = () => {
+  const [data, setData] = uDeState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+};
